cef1c24758f2dcefb12a7567116bac4a027a4544
Thomas Castiglione <thomas@agcom.com.au>
2016-12-15 20:55:04 +0800

    experiment with a special binder which reduces size

--- src/Serialize.Linq.Tests/Internals/Json.cs
+++ src/Serialize.Linq.Tests/Internals/Json.cs
@@ -1,6 +1,10 @@
 ﻿using Newtonsoft.Json;
 using Serialize.Linq.Nodes;
 using System.Linq.Expressions;
+using System;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Linq;
 
 namespace Serialize.Linq.Tests
 {
@@ -8,7 +12,8 @@ namespace Serialize.Linq.Tests
     {
         private static readonly JsonSerializerSettings _settings = new JsonSerializerSettings
         {
-            TypeNameHandling = TypeNameHandling.All,       
+            TypeNameHandling = TypeNameHandling.All,
+            Binder = new DataContractBinder()
         };
 
         public static string Serialize(Expression expression)
@@ -26,6 +31,31 @@ namespace Serialize.Linq.Tests
         public static ExpressionNode ToExpressionNode(this Expression expression)
         {
             return ExpressionNode.FromExpression(expression);
+        }
+
+        private class DataContractBinder : SerializationBinder
+        {
+            public override Type BindToType(string assemblyName, string typeName)
+            {
+                if (assemblyName == "SL") assemblyName = "Serialize.Linq, Version=2.0.0.0, Culture=neutral, PublicKeyToken=null";
+                var a = Assembly.Load(new AssemblyName(assemblyName));
+                var result = a.GetTypes().Where(t => DCName(t) == typeName).SingleOrDefault();
+                return result ?? Type.GetType(Assembly.CreateQualifiedName(assemblyName, typeName));
+            }
+
+            public override void BindToName(Type serializedType, out string assemblyName, out string typeName)
+            {
+                assemblyName = serializedType.GetTypeInfo().Assembly.FullName;
+                if (assemblyName == "Serialize.Linq, Version=2.0.0.0, Culture=neutral, PublicKeyToken=null") assemblyName = "SL";
+                typeName = DCName(serializedType) ?? serializedType.FullName;
+            }
+
+            private string DCName(Type t)
+            {
+                return t.GetTypeInfo().GetCustomAttribute<DataContractAttribute>()?.Name ??
+                       t.GetTypeInfo().GetCustomAttribute<CollectionDataContractAttribute>()?.Name ??
+                       t.FullName;
+            }
         }
     }
 }
